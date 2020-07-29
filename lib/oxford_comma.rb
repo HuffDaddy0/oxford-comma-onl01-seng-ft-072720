47851505e7223ccf7a42456413cf06abc980b11d
Tyler Huffman <ty.huff24@gmail.com>
2020-07-29 16:51:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  new_array = []
+  array.each_with_index do |element, index|
+    if array.length > 1 && index == array.length - 1
+      element = "and #{element}"
+      new_array.push(element)
+    else
+      new_array.push(element)
+    end
+  end
+  if array.length > 2
+    new_array.join(", ")
+  else
+    new_array.join(" ")
+  end
 end
